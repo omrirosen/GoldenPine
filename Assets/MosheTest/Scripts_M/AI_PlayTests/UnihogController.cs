fa18851a65a144a2e85563194947321848f4df97
Moshe-Paz <57709077+Moshe-Paz@users.noreply.github.com>
2020-10-06 16:04:46 +0300

    Moshe 6.10
    Sprites: add Unihog
    animation clip => Enemies => Unihog animations
    animation controllers=> Enemies=>Unihog
    moshe test => new folders => new script Unihog controller

---
+++ Assets/MosheTest/Scripts_M/AI_PlayTests/UnihogController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UnihogController : MonoBehaviour
+{
+    [SerializeField] public enum state_Machine {Idle, SeeTarget, Attack }
+    [SerializeField] public SpriteRenderer GFX;
+    private state_Machine state;
+    public float speed = 5f;
+
+    private void Start()
+    {
+        state = state_Machine.Idle;
+    }
+
+    private void Update()
+    {
+        AI_StatsControll();
+    }
+
+    public void AI_StatsControll()
+    {
+        switch (state)
+        {
+            case state_Machine.Idle:
+            //    transform.DOMoveX(transform.position.x - 10f, 2f*speed*Time.deltaTime);
+                break;
+            case state_Machine.SeeTarget:
+                break;
+            case state_Machine.Attack:
+                break;
+            default:
+                break;
+        }
+    }
+}
+
