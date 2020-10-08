21e23cced1a2f98a9c1f83fcc90890e645cc7708
farkrash <farkrash@gmail.com>
2020-10-08 22:13:27 +0300

    Playerstats and dmg to playerhealth
    new script containing player health and dmg functions

---
+++ Assets/TomerTest/scripts/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerStats : MonoBehaviour
+{
+    private int playerHealth = 4;
+    [SerializeField] private GameObject PlayerSelf;
+    
+    public void TakeDmg(int Dmg)
+    {
+        playerHealth -= Dmg;
+        Debug.Log(playerHealth);
+        Debug.Log(Dmg);
+
+        if (playerHealth <= 0)
+        {
+            Die();
+
+        }
+
+        
+    }
+    private void Die()
+    {
+        PlayerSelf.SetActive(false);
+    }
+}
---
+++ Assets/TomerTest/scripts/TakeDmg.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDmg : MonoBehaviour
+{
+    private PlayerStats PS;
+    private GameObject player;
+    private void Awake()
+    {
+        player = GameObject.Find("Player With Shield");
+       PS = player.GetComponent<PlayerStats>();
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PS.TakeDmg(1);
+        }
+    }
+}
