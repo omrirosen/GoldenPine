9ec03239917511f64aa1bfc56c0db139af27ff5d
farkrash <farkrash@gmail.com>
2020-10-10 23:38:24 +0300

    health UI and healing
    added health Ui and animations
    also added healing and heal animation(in UI)

--- Assets/TomerTest/scripts/PlayerStats.cs
+++ Assets/TomerTest/scripts/PlayerStats.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class PlayerStats : MonoBehaviour
 {
     private int playerHealth = 4;
     [SerializeField] private GameObject PlayerSelf;
+    [SerializeField] private Animator animator;
     
     public void TakeDmg(int Dmg)
     {
         playerHealth -= Dmg;
         Debug.Log(playerHealth);
-        Debug.Log(Dmg);
+
+        animator.SetInteger("PlayerHealthUI", playerHealth);
 
         if (playerHealth <= 0)
         {
@@ -26,4 +29,36 @@ public class PlayerStats : MonoBehaviour
     {
         PlayerSelf.SetActive(false);
     }
+    
+    public void HealPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (playerHealth == 1)
+            {
+                animator.Play("IncreaseHealth");
+                playerHealth = 4;
+            }
+
+            if(playerHealth == 2)
+            {
+                animator.Play("IncreaseHealth_2");
+                playerHealth = 4;
+            }
+
+            if(playerHealth == 3)
+            {
+                animator.Play("IncreaseHealth_3");
+                playerHealth = 4;
+            }
+            animator.SetInteger("PlayerHealthUI", playerHealth);
+        }
+    }
+
+    private void Update()
+    {
+        HealPlayer();
+    }
+
+
 }
