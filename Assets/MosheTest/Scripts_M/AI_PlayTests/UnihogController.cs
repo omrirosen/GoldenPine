1dc6c01825427939975646a54a40d35565b31f99
Moshe-Paz <57709077+Moshe-Paz@users.noreply.github.com>
2020-10-12 18:15:02 +0300

    Update to unihog
    add new layer for ai (physics )
    finish working with DOtween (not fill like i wanted), will try to make new script using rigidbody

--- Assets/MosheTest/Scripts_M/AI_PlayTests/UnihogController.cs
+++ Assets/MosheTest/Scripts_M/AI_PlayTests/UnihogController.cs
@@ -29,7 +29,9 @@ public class UnihogController : MonoBehaviour
     private Transform target_Pos;
     public bool seeTarget = false;
     public bool backTo_Idle = false;
- 
+    private bool bacttoIdle_Tween = false;
+   
+
     // --- Chase/Attack-----//
 
     private void Start()
@@ -48,69 +50,101 @@ public class UnihogController : MonoBehaviour
 
     private void Update()
     {
-        PickState(); // set the state 
+       
         AI_StatsControll(); // controll each state , what will it do 
         StartCoroutine(FindMyDirection()); // find ai look direction and flip GFX
-
-
+        if (target_Pos != null && Mathf.Abs((target_Pos.position - transform.position).magnitude) < 5.5f)
+        {
+            
+            seeTarget = true;
+        }
+        if(Chase_tween!=null)
+        {
+            print(Chase_tween.IsPlaying());
+        }
 
     }
 
-    public void PickState()
-    {
-        if(seeTarget)
-        {
-            state = state_Machine.Attack;
-        }
-        else
-        {
-            state = state_Machine.Idle;
-        }
-        if(backTo_Idle)
-        {
-            state = state_Machine.BackToIdle;
-        }
-        else
-        {
-            state = state_Machine.Idle;
-        }
-    }
+   
 
     public void AI_StatsControll()
     {
         switch (state)
         {
             case state_Machine.Idle:
-                if (!Patroltween_isactive)
-                {
-                    // do once
-                    Patroltween_isactive = true;
-                    AI_patroll_tween = transform.DOLocalPath(path, speed, PathType.Linear, PathMode.Sidescroller2D)
-                        .SetEase(Ease.Flash);
-                    animator.SetBool("IsMoving", Patroltween_isactive);
-
-                }
-                if (!AI_patroll_tween.IsPlaying())
+                if (!seeTarget)
                 {
                     
-                    Patroltween_isactive = false;
-                    animator.SetBool("IsMoving", Patroltween_isactive);
+                    if (!Patroltween_isactive)
+                    {
+                        // do once
+                        Patroltween_isactive = true;
+                        AI_patroll_tween = transform.DOLocalPath(path, speed, PathType.Linear, PathMode.Sidescroller2D)
+                            .SetEase(Ease.Flash);
+                        animator.SetBool("IsMoving", Patroltween_isactive);
+
+                    }
+                    if( AI_patroll_tween.position==path[1].x)
+                    {
+                        print("yay");
+                    }
+                    if (!AI_patroll_tween.IsPlaying())
+                    {
+
+                        Patroltween_isactive = false;
+                        animator.SetBool("IsMoving", Patroltween_isactive);
+                    }
                 }
-     
+                else
+                {
+                    AI_patroll_tween.Kill();
+                    state = state_Machine.Attack;
+                }
                 break;
         
             case state_Machine.Attack:
+                if (seeTarget)
+                {
+                    
+                    
+                  Chase_tween = transform.DOLocalMoveX(target_Pos.position.x, speed / 2f).SetEase(Ease.Flash);
 
-                Chase_tween = transform.DOLocalMoveX(target_Pos.position.x, speed/2f).SetEase(Ease.Flash);
-                // todo animations
+                  if (Mathf.Abs((target_Pos.position - transform.position).magnitude) > 6f)
+                  {
+                  seeTarget = false;
+                  }
+                        // todo animations
+                    
+                }
+                else
+                {
+                    StartCoroutine(ForgetTarget());
+                    state = state_Machine.BackToIdle;
+                }
                 break;
 
             case state_Machine.BackToIdle:
-                backToIdle_tween=transform.DOLocalMoveX(path[0].x, speed / 4f).SetEase(Ease.Flash);
-                //if(transform.position.x==(path[0].x-1f) || transform.position.x == (path[0].x + 1f))
-                //{
-                //    backTo_Idle = false; todo
-                //}
+                if (!seeTarget)
+                {
+                   
+                    if (!bacttoIdle_Tween)
+                    {
+                        
+                        backToIdle_tween = transform.DOLocalMoveX(path[0].x, speed / 4f).SetEase(Ease.Flash);
+                        if (Mathf.Abs(path[0].x - transform.position.x) < 0.2f)
+                        {
+                            backToIdle_tween.Kill();
+                            state = state_Machine.Idle;
+                            bacttoIdle_Tween = true;
+                        }
+                    }
+                }
+                else
+                {
+                    backToIdle_tween.Kill();
+                    bacttoIdle_Tween = false;
+                    state = state_Machine.Attack;
+                }
 
                 break;
         }
@@ -124,26 +158,36 @@ public class UnihogController : MonoBehaviour
     {
         if(collision.gameObject.tag=="Player")
         {
-            seeTarget = true;
+           // seeTarget = true;
             target_Pos = collision.transform;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+           
+        }
+        else if(collision.gameObject.tag=="Block")
+        {
+           
+            if (Chase_tween!=null)
+            {
+                print("kill");
+
+                StartCoroutine(ForgetTarget());
+            }
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            target_Pos = collision.transform;
-            StartCoroutine(ForgetTarget());
-        }
-    }
+    
 
     public IEnumerator ForgetTarget()
     {
         yield return new WaitForSeconds(.5f);       
         Chase_tween.Kill();
         seeTarget = false;
-        backTo_Idle = true;
+       // backTo_Idle = true;
 
     }
 
