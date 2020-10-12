0cade8deef21e738cb0a7a00d6a2202ffacc3773
Moshe-Paz <57709077+Moshe-Paz@users.noreply.github.com>
2020-10-13 01:30:08 +0300

    Add new unihog script+ prefab using rigidbody ( working great will continue with this one)

---
+++ Assets/MosheTest/Scripts_M/AI_PlayTests/Unihog1Controller.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unihog1Controller : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Animator animator;
+    public bool isTurning = false;
+
+
+    Rigidbody2D rb2d;
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isTurning)
+        {
+            if (IsFacingRight())
+            {
+                rb2d.velocity = new Vector2(moveSpeed, 0);
+                
+                animator.SetBool("IsMoving", true);
+
+            }
+            else
+            {
+                rb2d.velocity = new Vector2(-moveSpeed, 0);
+                animator.SetBool("IsMoving", true);
+            }
+        }
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+            animator.SetBool("IsMoving", false);
+        }
+    }
+
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x > -Mathf.Epsilon;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        if (!isTurning)
+        {
+            StartCoroutine(Turn());
+            transform.localScale = new Vector2(-(Mathf.Sign(rb2d.velocity.x)), transform.localScale.y);
+        }
+    }
+
+    IEnumerator Turn()
+    {
+        isTurning = true;
+        animator.SetBool("IsTurning", isTurning);
+        yield return new WaitForSeconds(0.3f);  
+        isTurning = false;
+        animator.SetBool("IsTurning", isTurning);
+    }
+
+}
