ab7ee07ee28eb96debb55fc133c9dca3a3ec18dd
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2020-10-16 16:36:05 +0300

    Added Parallax Effect and Backgrounds

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+    [SerializeField] private bool doIDuplicateVertically = false;
+    private Transform cameraTransform;
+    private Vector3 lastCameraPos;
+    private float textureUnitSizeX;
+    private float textureUnitSizeY;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPos = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPos;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x , deltaMovement.y * parallaxEffectMultiplier.y);
+        lastCameraPos = cameraTransform.position;
+
+        if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offsetPosX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPosX, transform.position.y);
+        }
+
+        if (Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY && doIDuplicateVertically)
+        {
+            float offsetPosY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
+            transform.position = new Vector3(transform.position.x, cameraTransform.position.y + offsetPosY);
+        }
+    }
+}
