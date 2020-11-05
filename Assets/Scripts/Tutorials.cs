5621ac8b0de3b89a8aeb1a7a15e6e790a9490afb
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2020-11-05 11:01:10 +0200

    Started Jump Tutorial

---
+++ Assets/Scripts/Tutorials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorials : MonoBehaviour
+{
+    [SerializeField] private GameObject jumpTutorialCanvas;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        jumpTutorialCanvas.SetActive(true);
+    }
+}
