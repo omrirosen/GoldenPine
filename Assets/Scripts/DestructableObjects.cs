fd867ae8c1f4e055cad395a80a8af6138e89034d
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2020-11-10 13:51:08 +0200

    Fixed Parallax + Destructable Rock + Dash attack tutorial

---
+++ Assets/Scripts/DestructableObjects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class DestructableObjects : MonoBehaviour
+{
+   
+    [SerializeField] private GameObject destructionEffect;
+    
+    
+    public void HandleDestruction()
+    {
+        Instantiate(destructionEffect, transform.position, quaternion.identity);
+            Destroy(gameObject);
+    }
+}
